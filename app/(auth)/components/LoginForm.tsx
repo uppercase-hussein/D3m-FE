a7b060a2bf9675001dd93014348317291d8e1c25
kamsiOrazulike <kamsi_orazulike@icloud.com>
2023-08-30 17:40:02 +0100

    login card update

--- app/(auth)/components/LoginForm.tsx
+++ app/(auth)/components/LoginForm.tsx
@@ -7,35 +7,11 @@ interface Tab {
 }
 
 const LoginForm = () => {
-  /* Tab functionality  - REMOVE */
-  const [activeTab, setActiveTab] = useState<Tab>({
-    title: "Staff",
-    role: "staff",
-  });
-  const handleTabClick = (tab: Tab) => {
-    setActiveTab(tab);
-  };
-
   return (
     <>
       <div className="border-b border-gray-200 mb-4">
         <div className="flex justify-center transition-all duration-200 ease-in">
-          <div
-            className={`px-4 py-2 cursor-pointer ${
-              activeTab.role === "staff" ? "text-red-500" : "text-gray-500"
-            }`}
-            onClick={() => handleTabClick({ title: "Staff", role: "staff" })}
-          >
-            Outlet
-          </div>
-          <div
-            className={`px-4 py-2 cursor-pointer ${
-              activeTab.role === "admin" ? "text-red-500" : "text-gray-500"
-            }`}
-            onClick={() => handleTabClick({ title: "Admin", role: "admin" })}
-          >
-            Admin
-          </div>
+          <div className="px-4 py-2 cursor-pointer text-gray-400">Please enter your login details</div>
         </div>
       </div>
 
@@ -48,11 +24,6 @@ const LoginForm = () => {
         <div className="text-center">
           <h1 className="text-3xl font-bold mx-auto mb-8 text-gray-800">
             D<sup className="text-red-500">3</sup>M
-            {activeTab.role === "admin" && (
-              <span className="text-xs font-semibold text-red-500 uppercase mx-2">
-                Admin
-              </span>
-            )}
           </h1>
         </div>
         <div className="relative w-full mb-4">
@@ -60,7 +31,7 @@ const LoginForm = () => {
             type="number"
             // onChange={(e) => handleChange("staffId", e.target.value)}
             className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-            placeholder="Staff ID"
+            placeholder="Username"
             required
           />
         </div>
@@ -74,17 +45,6 @@ const LoginForm = () => {
               required
             />
           </div>
-          {activeTab.role === "admin" && (
-            <div className="relative w-full ml-2">
-              <input
-                type="password"
-                // onChange={(e) => handleChange("adminPassword", e.target.value)}
-                className="border-0 px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                placeholder="* Admin Password"
-                required
-              />
-            </div>
-          )}
         </div>
 
         <div>
@@ -105,7 +65,7 @@ const LoginForm = () => {
             className="bg-red-600 text-white active:bg-red-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg hover:bg-red-500 outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
             type="submit"
           >
-            Sign In
+            Login
           </button>
         </div>
       </form>
